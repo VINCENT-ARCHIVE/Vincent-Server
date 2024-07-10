c2684f8e61f66d97f431e6d472066256a071ffd6
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-07-10 19:30:49 +0900

    :sparkles: Spring Security 설정

---
+++ src/main/java/com/vincent/config/security/provider/JwtProvider.java
@@ -0,0 +1,102 @@
+package com.vincent.config.security.provider;
+
+import com.vincent.exception.handler.JwtExpiredHandler;
+import com.vincent.exception.handler.JwtInvalidHandler;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider implements InitializingBean {
+    @Value("${jwt.token.secret}")
+    private String secret;
+    private static SecretKey secretKey;
+    private static final Long expireMs = 1000L * 60; //30분
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(secret);
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public  String getMemberId(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("memberId", String.class);
+    }
+
+    public  String getEmail(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("email", String.class);
+    }
+
+    public  String getRole(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+
+    public  Boolean isExpired(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public  String createJwt(Long memberId, String email){
+        return Jwts.builder()
+                .claim("memberId", memberId)
+                .claim("email", email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+expireMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public  String createJwt(String email){
+        return Jwts.builder()
+                .claim("email", email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+expireMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public  void validateAccessToken(String accessToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(accessToken)
+                    .getPayload()
+                    .getExpiration();
+        } catch (ExpiredJwtException e) {
+            throw new JwtExpiredHandler("Expired Token Exception");
+        } catch (UnsupportedJwtException | SecurityException | MalformedJwtException | NullPointerException e) {
+            throw new JwtInvalidHandler("Invalid Token Exception");
+        }
+    }
+
+}
