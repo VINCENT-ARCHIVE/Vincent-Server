a48d39414c24bc1edf8680b37bc3f2fc25aa86fe
bigwaveBigwave <bluebiird909@naver.com>
2024-08-07 18:58:22 +0900

    :sparkles: 개별 콘센트 조회 기능

--- src/main/java/com/vincent/domain/socket/controller/dto/SocketResponseDto.java
+++ src/main/java/com/vincent/domain/socket/controller/dto/SocketResponseDto.java
@@ -1,5 +1,25 @@
 package com.vincent.domain.socket.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class SocketResponseDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SocketInfo {
+
+        Long socketId;
+        String socketName;
+        String socketImage;
+        String buildingName;
+        String spaceName;
+        Boolean isBookmarkExist;
+
+    }
 
 }
--- src/main/java/com/vincent/domain/socket/converter/SocketConverter.java
+++ src/main/java/com/vincent/domain/socket/converter/SocketConverter.java
@@ -1,5 +1,19 @@
 package com.vincent.domain.socket.converter;
 
+import com.vincent.domain.socket.controller.dto.SocketResponseDto;
+import com.vincent.domain.socket.entity.Socket;
+
 public class SocketConverter {
+
+    public static SocketResponseDto.SocketInfo toSocketInfoResponse(Socket socketInfo, Boolean isBookmarkExist) {
+        return SocketResponseDto.SocketInfo.builder()
+            .socketId(socketInfo.getId())
+            .socketName(socketInfo.getName())
+            .socketImage(socketInfo.getImage())
+            .buildingName(socketInfo.getSpace().getFloor().getBuilding().getName())
+            .spaceName(socketInfo.getSpace().getName())
+            .isBookmarkExist(isBookmarkExist)
+            .build();
+    }
 
 }
