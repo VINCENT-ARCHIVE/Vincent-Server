206f72a24c00998303fbc7c38808bea845b6b8f9
bigwaveBigwave <bluebiird909@naver.com>
2024-08-16 23:59:51 +0900

    :sparkles: 층 소켓 조회 api

--- src/main/java/com/vincent/domain/socket/converter/SocketConverter.java
+++ src/main/java/com/vincent/domain/socket/converter/SocketConverter.java
@@ -2,6 +2,8 @@ package com.vincent.domain.socket.converter;
 
 import com.vincent.domain.socket.controller.dto.SocketResponseDto;
 import com.vincent.domain.socket.entity.Socket;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SocketConverter {
 
@@ -13,6 +15,22 @@ public class SocketConverter {
             .buildingName(socketInfo.getSpace().getFloor().getBuilding().getName())
             .spaceName(socketInfo.getSpace().getName())
             .isBookmarkExist(isBookmarkExist)
+            .build();
+    }
+
+    public static SocketResponseDto.SocketLocation toSocketLocation(Socket socket) {
+        return SocketResponseDto.SocketLocation.builder()
+            .socketId(socket.getId())
+            .xCoordinate(socket.getXCoordinate())
+            .yCoordinate(socket.getYCoordinate())
+            .build();
+    }
+
+    public static SocketResponseDto.SocketLocationList toSocketLocationList(List<Socket> socketList) {
+        List<SocketResponseDto.SocketLocation> a = socketList.stream()
+            .map(SocketConverter::toSocketLocation).collect(Collectors.toList());
+        return SocketResponseDto.SocketLocationList.builder()
+            .locationList(a)
             .build();
     }
 
--- src/main/java/com/vincent/domain/socket/service/SocketService.java
+++ src/main/java/com/vincent/domain/socket/service/SocketService.java
@@ -1,9 +1,16 @@
 package com.vincent.domain.socket.service;
 
 import com.vincent.apipayload.status.ErrorStatus;
+import com.vincent.domain.building.entity.Building;
+import com.vincent.domain.building.entity.Floor;
+import com.vincent.domain.building.entity.Space;
+import com.vincent.domain.building.repository.BuildingRepository;
+import com.vincent.domain.building.repository.FloorRepository;
+import com.vincent.domain.building.repository.SpaceRepository;
 import com.vincent.domain.socket.entity.Socket;
 import com.vincent.domain.socket.repository.SocketRepository;
 import com.vincent.exception.handler.ErrorHandler;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +21,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class SocketService {
 
     private final SocketRepository socketRepository;
+    private final BuildingRepository buildingRepository;
+    private final FloorRepository floorRepository;
+    private final SpaceRepository spaceRepository;
 
     public Socket getSocketInfo(Long socketId) {
 
         return  socketRepository.findById(socketId).orElseThrow(() -> new ErrorHandler(ErrorStatus.SOCKET_NOT_FOUND));
+    }
+
+    public List<Socket> getSocketLocationList(Long buildingId, int level) {
+
+        Building a = buildingRepository.findById(buildingId).orElseThrow(()
+            -> new ErrorHandler(ErrorStatus.BUILDING_NOT_FOUND));
+
+        Floor b = floorRepository.findByBuildingAndLevel(a, level);
+
+        List<Space> s = spaceRepository.findAllByFloor(b);
+
+        List<Socket> socket = null;
+
+        for (Space space : s) {
+
+            socket = socketRepository.findAllBySpace(space);
+        }
+
+
+        return  socket;
     }
 
 }
