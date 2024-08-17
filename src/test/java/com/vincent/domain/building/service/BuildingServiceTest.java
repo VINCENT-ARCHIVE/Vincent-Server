91f7936a38b15ec84df96772c5e12529bf56d478
bigwaveBigwave <bluebiird909@naver.com>
2024-08-17 15:56:54 +0900

    :white_check_mark: 1. service : 마지막 space에 대한 socket만 조회되는 문제 해결 2. controller : buildingId RequestParam으로 변경 3. converter : socketList 변수명 변경

--- src/test/java/com/vincent/domain/building/service/BuildingServiceTest.java
+++ src/test/java/com/vincent/domain/building/service/BuildingServiceTest.java
@@ -1,4 +1,4 @@
-package com.vincent.domain.bookmark.service;
+package com.vincent.domain.building.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
